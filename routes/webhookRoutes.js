55fb2f009fddc9b36af27b9942a4f8405d34e560
Jaume Fernández <jaumefernandez@carnicassunyer.com>
2024-10-26 11:43:31 +0200

    arreglo de rutes

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Order = require('../models/Order');
+const Order = require('../models/Orders');
 
 router.post(
   '/',
